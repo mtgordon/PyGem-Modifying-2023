b9f6e402860a6bd6522e6b92cbd88d97a3f3c1a1
eliBro22 <elijahd.brown@calbaptist.edu>
2023-05-12 11:35:54 -0700

    Automate_febio runs with single material, can do multiple variable rows

--- FeBio Inverse/Automate_Febio.py
+++ FeBio Inverse/Automate_Febio.py
@@ -3,6 +3,7 @@ This file is responsible for running FeBio in conjunction with the feb_variables
 a run is completed for each row
 """
 import csv
+import sys
 import subprocess
 import xml.etree.ElementTree as ET
 
@@ -11,15 +12,17 @@ Function: RunFEBinFeBio
 Takes in the geometry feb file and material properties feb file, along with the location of FeBio
 on the system and runs it via the command line. The output log file name can also be given.
 '''
-def RunFEBinFeBio(geometryFileName, propertiesFileName, prop2, FeBioLocation, outputFileName=None):
-    CallString = FeBioLocation + ' -i ' + geometryFileName + ' -s ' + propertiesFileName + ' ' + prop2
+def RunFEBinFeBio(geometryFileName, propertiesFileName, FeBioLocation, outputFileName=None):
+    CallString = FeBioLocation + ' -i ' + geometryFileName + ' -s ' + propertiesFileName
     if outputFileName is not None:
-        CallString += ' -o ' + 'ENTER_MAT_PROP_HERE' + '.log'
+        CallString += ' -o ' + outputFileName
     print(CallString)
     subprocess.call(CallString)
 
+
 dictionary_file = 'feb_variables.csv'
-FeBioLocation = 'C:\Program Files\FEBioStudio2\\bin\\febio4.exe'
+FeBioLocation = 'C:\\Program Files\\FEBioStudio2\\bin\\febio4.exe'
+originalFebFilename = 'op04.feb'
 
 #Get data from the Run_Variables file
 # Newer code (2/14)
@@ -35,20 +38,44 @@ first_iter = True
 
 #Have the default material variables be 1 (100%) so they do not change if no variable is given
 default_dict = {
-    'Material1': 1,
-    'Material2': 1,
-    'Material3': 1
+    'Mat1': 1,
+    'Mat2': 1,
+    'Mat3': 1
 }
 
-# tree = ET.parse('op04.feb')
-# root = tree.getroot()
-# for point in root.iter('point'):
-#     print(child.tag, child.attrib)
+current_run_dict = default_dict.copy()
 
 for row in DOE_dict:
     print('Row:', row)
 
+    # Initialize current run dictionary
+    for key in DOE_dict.fieldnames:
+        if key in default_dict.keys():
+            current_run_dict[key] = row[key]
+        else:
+            if key != 'Run Number':
+                print(key, 'is an invalid entry')
+                sys.exit()
+
+    #generation of current run file template based on attributes
+    fileTemplate = ''
+    for key in current_run_dict:
+        param = '_' + str(key) + '(' + str(current_run_dict[key]) + ')'
+        fileTemplate += param
+
+    # Parse original FEB file and update coordinates to new FEB file
+    tree = ET.parse(originalFebFilename)
+    root = tree.getroot()
+    for point in root.iter('point'):
+        print(point.text)
+        textList = point.text.split(', ')  # 0 is first coord (no change), 1 is second coord (change)
+        newCoord = float(textList[1]) * float(current_run_dict['Mat1'])
+        point.text = textList[0] + ', ' + str(newCoord)
+
+    # using UTF-8 encoding does not bring up any issues (can be changed if needed)
+    workingFebFilename = fileTemplate + '.feb'
+    tree.write(workingFebFilename, xml_declaration=True, encoding='UTF-8')
 
     # TODO: to easily get input files from anywhere, do the shutil move to working directory
-    # RunFEBinFeBio('oi04.feb', 'op04.feb', FeBioLocation)
-
+    logFile = fileTemplate + '.log'
+    RunFEBinFeBio('oi04.feb', workingFebFilename, FeBioLocation, logFile)
