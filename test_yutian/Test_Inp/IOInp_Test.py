777845addd5ad725a76c6703532ef041dcec15ba
Yutian Yang <70208693+YutianYang709631@users.noreply.github.com>
2023-05-19 09:56:41 -0700

    Add files via upload

--- test_yutian/Test_Inp/IOInp_Test.py
+++ test_yutian/Test_Inp/IOInp_Test.py
@@ -3,7 +3,7 @@ from test_yutian.functions import IOfunctions_Inp as io
 file_name = "../Normal_Generic_copy.inp"
 part_name = "OPAL325_AVW_v6"
 
-nodes_list = [1,2,5]
+# nodes_list = [1,2,5]
 
 # points_list = [
 #     (11, 2, 3),
@@ -21,10 +21,39 @@ nodes_list = [1,2,5]
 #     131
 # ]
 
-# Test the writing funciton
+"""
+TEST: io.write_Nset_to_inp_file
+"""
 # io.write_Nset_to_inp_file(file_name, "OPAL325_ATFP", 999, "AWP_dis", Nset_list)
 
-## Test the writting fucntion
+"""
+TEST: io.write_new_part_to_inp_file
+"""
 # io.write_new_part_to_inp_file(file_name, part_name, "element", points_list, element_list)
 
-print(io.extractNodesFromINP(file_name, part_name, nodes_list))
+"""
+TEST: io.addToVals
+"""
+# connections = [[], [], [], []]
+# nums = [1, 2, 3, 4]
+# updated_connections = io.addToVals(connections, nums)
+# print(updated_connections)
+
+
+"""
+TEST: io.get_interconnections
+"""
+connection_list = io.get_interconnections(file_name, part_name)
+for connection in connection_list:
+    print(connection)
+
+
+"""
+TEST: io.write_points_below_excluded_to_inp
+"""
+# filtered_points, filtered_count, total_count = io.get_points_below(file_name, part_name, 37)
+# print("Filtered points number: ", filtered_points)
+# print("Filtered count: ", filtered_count)
+# print("Total count: ", total_count)
+#
+# io.write_points_below_excluded_to_inp(file_name, part_name, 37, 999, "AWP")
