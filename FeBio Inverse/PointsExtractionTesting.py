9c314db438f59dd9401f6f698103dc9ecc91e3eb
EGRstudent <COBScomputer@gmail.com>
2024-04-29 11:46:14 -0700

    Created determineRadiiFromFEB function in PointsExtractionTesting.py which determines the inner & outer radius of the cylinder object in the .feb file and returns the points map of the generated cylinder

--- FeBio Inverse/PointsExtractionTesting.py
+++ FeBio Inverse/PointsExtractionTesting.py
@@ -11,7 +11,7 @@ import math
 from pygem import RBF
 
 
-febio_file_name = "Basic_Cylider.feb"
+febio_file_name = "D:\\Gordon\\Automate FEB Runs\\2024_4_29 auto\\Base File\\Basic_Cylinder_Pressure.feb"
 node_name = "Object5"
 extract_points = IOfunctions.extract_coordinates_list_from_feb(febio_file_name, node_name)
 print(extract_points)
@@ -87,20 +87,47 @@ def plot_3d_points(points):
 
    plt.show()
 
-# Parameters for Cylinder Creation
+#TODO: Input Parameters for Cylinder Creation
 height = 4
 num_points = 200
 
-#TODO: This creates the cylinder from the .feb file, but is hard coded, we want this to create it from parsing the .feb file
-cylinder1points = generate_annular_cylinder_points(1, 2, height, num_points)
-#generate_annular_cylinder_points(1, 2, height, num_points)
+
 #plot_3d_points(cylinder1points)
+
+"""
+This method utilizes the .feb files extracted points which are parsed from the .feb file previously using
+IOfunctions.extract_coordinates_list_from_feb, We then convert this to a np array and take the first
+two nodes x-values which are the inner and outer radius correspondingly.
+
+TEST INPUT: [(x1, y1, z1), (x2, y2, z2)]
+TEST OUTPUT: [Inner_Radius, Outer_Radius]
+"""
+def determineRadiiFromFEB(extracted_points):
+    # convert array containing tuples to np array
+    extract_points = np.array(extracted_points)
+
+    for i in range(2):  # Loop over the range from 0 to 1 (inclusive)
+        if i == 0:
+            inner_radius = extract_points[i][0]
+            print("Inner Radius = " + str(inner_radius))
+
+        if i == 1:
+            outer_radius = extract_points[i][0]
+            print("Outer Radius = " + str(outer_radius))
+
+    # create cylinder using our found inner & outer radius
+    cylinderpoints = generate_annular_cylinder_points(inner_radius, outer_radius, height, num_points)
+
+    return cylinderpoints
+
+cylinder1points = determineRadiiFromFEB(extract_points)
 
 # Cylinder which we are morphing. This is the Cylinder that MATTERS!!
 cylinder2points = generate_annular_cylinder_points(2,3,height,num_points)
 #plot_3d_points(cylinder2points)
 
 #TODO: This uses RBF Interpolator from the SciPy Library, Currently unused, because we are using PyGem
+
 # rbf = RBF(original_control_points = cylinder1points, deformed_control_points = cylinder2points, func='thin_plate_spline', radius = 10)
 # rbf(cylinder1points)
 # Initialize the RBF with the original control points and their deformations
